6f96bf7dd2410ad05379481976e13a64b7f448c4
Jaime Aznar <jaimeaznar@Jaimes-MacBook-Pro.local>
2025-08-15 13:44:56 +0200

    updating values

--- modules/github-repository/main.tf
+++ modules/github-repository/main.tf
@@ -26,19 +26,19 @@ resource "github_repository" "repo" {
   # }
 }
 
-resource "github_branch_protection" "main" {
-  repository_id = github_repository.repo.node_id
-  pattern       = "main"
+# resource "github_branch_protection" "main" {
+#   repository_id = github_repository.repo.node_id
+#   pattern       = "main"
   
-  enforce_admins = true
+#   enforce_admins = true
   
-  required_status_checks {
-    strict = true
-  }
+#   required_status_checks {
+#     strict = true
+#   }
   
-  required_pull_request_reviews {
-    dismiss_stale_reviews           = true
-    require_code_owner_reviews      = true
-    required_approving_review_count = 1
-  }
-}
+#   required_pull_request_reviews {
+#     dismiss_stale_reviews           = true
+#     require_code_owner_reviews      = true
+#     required_approving_review_count = 1
+#   }
+# }
